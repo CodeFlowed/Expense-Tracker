143626e0d7ba66e7be6043ae3c24b0b3b1ab0ed6
cybermon <cybermon@CyberMon.local>
2021-02-10 20:15:15 -1000

    Add Flatpickr date picker js and the date input's default of today's date

--- app.js
+++ app.js
@@ -9,9 +9,21 @@ document.addEventListener("click", event => {
 
 function handleSubmitBtn () {
     //Access user's input values 
-    let name   = document.querySelector(".exp-input-name").value,
-        date   = document.querySelector(".exp-input-date").value,
-        amount = document.querySelector(".exp-input-amount").value; // Turn into a string with decimal value
+    let name   = document.querySelector(".exp-input-name").value.trim(),
+        date   = document.querySelector(".exp-input-date").value.trim();
+        amount = document.querySelector(".exp-input-amount").value.trim();
+
+    //If date is empty, make it be today's date
+    if (!date) { date = getTodaysDate(); }
+
+    // let amountCurrency;
+
+    //Create object for formatting for US currency
+    // let format = new Intl.NumberFormat('en-US', { 
+    //     style: "currency", 
+    //     currency: "USD", 
+    //     minimumFractionDigits: 2, 
+    // });
         
     //Get the table's tbody element
     let tbody = document.querySelector(".expense-table__tbody");
@@ -24,9 +36,26 @@ function handleSubmitBtn () {
             <td>$${amount}</td>
         </tr>`;
 
-    //Clear all the user's previous inputs from the webpage form
+    //Clear all the user's previous inputs from the webpage form 
+    clearExpenseFormInputs();
+}
+
+function clearExpenseFormInputs() {
     let inputs = document.querySelectorAll(".exp-form__input");
     inputs.forEach(input => {
         input.value = "";
     });
-}   
+}
+
+function getTodaysDate() {
+    let [month, day, year] = new Date().toLocaleDateString("en-US").split("/"); //Create variables month, day, and year w/ destructuring assignment syntax
+
+    // Pad month and date to start w/ 0s (example: 2 -> 02)
+    month = month.padStart(2, "0");
+    day = day.padStart(2, "0");
+
+    return `${year}-${month}-${day}`;
+}
+
+//Run Flatpickr date picker
+flatpickr(document.getElementById("exp-input-date"), {});
